3299c922ab3b8d35964e21432691a4f9fca4ffc3
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-01-04 01:17:55 -0800

    xyxy

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Function the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t x = 0;
+	size_t y = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			x = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			y = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((x > y) ? x : y);
+	}
+}
