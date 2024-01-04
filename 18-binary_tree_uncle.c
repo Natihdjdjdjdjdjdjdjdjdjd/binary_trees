3317c15a326e4c7579c34bf6b2608aaff38dbcd1
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-01-04 05:18:50 -0800

    aksksk

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -15,9 +15,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (node->parent->right);
 
 }
-/**
- * binary_tree_uncle - function that finds the uncle of a node.
- */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)
