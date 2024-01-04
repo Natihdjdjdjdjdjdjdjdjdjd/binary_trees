341bfb2c62a20a7f9cd098e2410ba2f096b29fd6
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-01-04 00:25:45 -0800

    dd

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - Free/delete an entire tree using recursion it
+ * @tree: the pointer to tree to free
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
+
+}
